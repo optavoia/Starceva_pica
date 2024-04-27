f04313a66e50760955ab09ca9c2b2a0afcbe56ba
Starceva <anzekikastar0806@gmail.com>
2024-04-27 21:54:33 +0300

    added variables

--- src/lvt/Menu.java
+++ src/lvt/Menu.java
@@ -1,10 +1,23 @@
 package lvt;
 
+import java.util.ArrayList;
+
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String izvele;
+		int izvelesIndekss;
+		String vards, uzvards, adress, numurs;
+		int vecums;
+		int diam = 0, cena = 0; //pie piccas
+		
+		String[] darbibas = {"User information", "Menu", "Order picca", "Make your own picca","Information","Aizvert Menu"};
+		String[] piccas = {"Peperoni", "Studentu", "Pikantā"};
+		String[] izvele1 = {"Cenas", "Menu"};
+		ArrayList<Object> piccaFromMenu = new ArrayList<Object>();
+		ArrayList<Object> YourOwnPicca = new ArrayList<Object>();
+		
+		
 	}
 
 }
