96785a26954fa3b7ad4cc20d24d38b470a70bc5e
Starceva <anzekikastar0806@gmail.com>
2024-04-28 12:07:56 +0300

    pabeidzu case 0

--- src/lvt/Menu.java
+++ src/lvt/Menu.java
@@ -10,15 +10,16 @@ public class Menu {
 	public static void main(String[] args) {
 		String izvele;
 		int izvelesIndekss;
-		String vards, uzvards, adress, numurs;
+		String vards, uzvards, adresse, numurs;
 		int vecums;
 		int diam = 0, cena = 0; //pie piccas
 		
-		String[] darbibas = {"User information", "Menu", "Order picca", "Make your own picca","Information","Aizvert Menu"};
+		String[] darbibas = {"New user", "User information", "Menu", "Order picca", "Make your own picca","Information","Aizvert Menu"};
 		String[] piccas = {"Peperoni", "Studentu", "Pikantā"};
 		String[] izvele1 = {"Cenas", "Menu"};
 		ArrayList<Object> piccaFromMenu = new ArrayList<Object>();
 		ArrayList<Object> YourOwnPicca = new ArrayList<Object>();
+		ArrayList<Object> User = new ArrayList<Object>();
 		
 		do {
 			izvele = (String)JOptionPane.showInputDialog(null, "Izvēlies darbību", "Izvēle", 
@@ -27,6 +28,20 @@ public class Menu {
 			
 			switch(izvelesIndekss) {
 			case 0:
+				vards = JOptionPane.showInputDialog("Ievadiet vardu: ");
+				uzvards = JOptionPane.showInputDialog("Ievadiet uzvardu: ");
+				adresse = JOptionPane.showInputDialog("Ievadiet savu adressi: ");
+				
+				int numLength;
+				do {
+					numurs = JOptionPane.showInputDialog("Ievadiet savu numuru: ");
+					numLength = numurs.length();
+				}while(numLength != 8);
+				
+				User cilveks = new User(vards, uzvards, adresse, numurs);
+				User.add(cilveks);
+				
+				JOptionPane.showMessageDialog(null, "Jūs esat veiksmīgi izveidojis kontu!","Paziņojums", JOptionPane.INFORMATION_MESSAGE);
 				break;
 			case 1:
 				break;
@@ -38,8 +53,10 @@ public class Menu {
 				break;
 			case 5:
 				break;
+			case 6:
+				break;
 			}
-		}while(izvelesIndekss != 5);
+		}while(izvelesIndekss != 6);
 		
 	}
 }
