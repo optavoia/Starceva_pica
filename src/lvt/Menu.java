13a4427b8bad67aaaf93b749f508ed966875d8a3
Starceva <anzekikastar0806@gmail.com>
2024-04-28 12:26:08 +0300

    made small changes

--- src/lvt/Menu.java
+++ src/lvt/Menu.java
@@ -14,12 +14,26 @@ public class Menu {
 		int vecums;
 		int diam = 0, cena = 0; //pie piccas
 		
-		String[] darbibas = {"New user", "User information", "Menu", "Order picca", "Make your own picca","Information","Aizvert Menu"};
+		String[] darbibas = {"User information", "Menu", "Order picca", "Make your own picca","Information","Aizvert Menu"};
 		String[] piccas = {"Peperoni", "Studentu", "Pikantā"};
 		String[] izvele1 = {"Cenas", "Menu"};
-		ArrayList<Object> piccaFromMenu = new ArrayList<Object>();
+		ArrayList<Object> picaFromMenu = new ArrayList<Object>();
 		ArrayList<Object> YourOwnPicca = new ArrayList<Object>();
 		ArrayList<Object> User = new ArrayList<Object>();
+		
+		vards = JOptionPane.showInputDialog("Ievadiet vardu: ");
+		uzvards = JOptionPane.showInputDialog("Ievadiet uzvardu: ");
+		adresse = JOptionPane.showInputDialog("Ievadiet savu adressi: ");
+		
+		int numLength;
+		do {
+			numurs = JOptionPane.showInputDialog("Ievadiet savu numuru: ");
+			numLength = numurs.length();
+		}while(numLength != 8);
+		
+		User cilveks = new User(vards, uzvards, adresse, numurs);
+		User.add(cilveks);
+		JOptionPane.showMessageDialog(null, "Jūs esat veiksmīgi izveidojis kontu!","Paziņojums", JOptionPane.INFORMATION_MESSAGE);
 		
 		do {
 			izvele = (String)JOptionPane.showInputDialog(null, "Izvēlies darbību", "Izvēle", 
@@ -28,20 +42,10 @@ public class Menu {
 			
 			switch(izvelesIndekss) {
 			case 0:
-				vards = JOptionPane.showInputDialog("Ievadiet vardu: ");
-				uzvards = JOptionPane.showInputDialog("Ievadiet uzvardu: ");
-				adresse = JOptionPane.showInputDialog("Ievadiet savu adressi: ");
-				
-				int numLength;
-				do {
-					numurs = JOptionPane.showInputDialog("Ievadiet savu numuru: ");
-					numLength = numurs.length();
-				}while(numLength != 8);
-				
-				User cilveks = new User(vards, uzvards, adresse, numurs);
-				User.add(cilveks);
-				
-				JOptionPane.showMessageDialog(null, "Jūs esat veiksmīgi izveidojis kontu!","Paziņojums", JOptionPane.INFORMATION_MESSAGE);
+				String bum ="";
+				bum += cilveks.izvadit();
+				JOptionPane.showMessageDialog(null, bum, "User",
+						JOptionPane.INFORMATION_MESSAGE);
 				break;
 			case 1:
 				break;
@@ -53,10 +57,8 @@ public class Menu {
 				break;
 			case 5:
 				break;
-			case 6:
-				break;
 			}
-		}while(izvelesIndekss != 6);
+		}while(izvelesIndekss != 5);
 		
 	}
 }
