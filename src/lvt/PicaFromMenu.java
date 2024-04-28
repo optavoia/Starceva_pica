17db2c47317d2a0a77dd4991eb5c3d14324260c2
Starceva <anzekikastar0806@gmail.com>
2024-04-28 11:45:11 +0300

    added constructor

---
+++ src/lvt/PicaFromMenu.java
@@ -0,0 +1,52 @@
+package lvt;
+
+public class PicaFromMenu {
+	private String name;
+	private int diam; //piccas izmers
+	private int cena;
+	private String toppings;
+	
+	public  PicaFromMenu(String name, int diam, int cena, String toppings) {
+		this.name = name;
+		this.diam = diam;
+		this.cena = cena;
+		this.toppings = toppings;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	void setName(String name){
+        this.name = name;
+    }
+	
+	public int getDiam() {
+		return diam;
+	}
+	
+	void setDiam(int diam) {
+		this.diam = diam;
+	}
+	
+	public int getCena() {
+		return cena;
+	}
+	
+	void setCena(int cena) {
+		this.cena = cena;
+	}
+	
+	public String getToppings() {
+		return toppings;
+	}
+	
+	void setToppings(String toppings){
+        this.toppings = toppings;
+    }
+	
+	public String izvadit() {
+		return  "\n\nNosaukums: "+getName()+
+				"\nToppings: "+getToppings();
+	}
+}
